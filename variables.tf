3aec5d98ea50f82955c499ccb1e4c609e1e0c798
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2024-01-29 08:50:38 -0600

    first commit

---
+++ variables.tf
@@ -0,0 +1,45 @@
+variable "common_tags" {
+  # default = {
+  #   Project     = "roboshop"
+  #   Environment = "dev"
+  #   Terraform   = "true"
+  # }
+}
+
+variable "tags" {
+  
+}
+
+variable "project_name" {
+  # default = "roboshop"
+}
+
+variable "environment" {
+  # default = "dev"
+}
+
+variable "zone_id" {
+  # type = string
+  # default = "Z1024693OKJAW10DDB2X"
+}
+
+variable "zone_name" {
+  # type = string
+  # default = "pavankumarmuvva.online"
+}
+
+variable "vpc_id" {
+  
+}
+
+variable "component_sg_id" {
+  
+}
+
+variable "private_subnet_ids" {
+  
+}
+
+variable "iam_instance_profile" {
+  
+}
